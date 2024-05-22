c0c685ce1f4cad137f56ac29a98035cb5b42be28
YehuditEr <y0556774655@gmail.com>
2024-05-22 12:17:03 +0300

    add example component into components folder

---
+++ src/components/example.tsx
@@ -0,0 +1,15 @@
+interface MyButtonProps {
+    /** The text to display inside the button */
+    title: string;
+    /** Whether the button can be interacted with */
+    disabled: boolean;
+
+}
+
+const MyButton = ({ title, disabled }: MyButtonProps) => {
+    return (
+        <button disabled={disabled}>{title}</button>
+    );
+}
+
+export default MyButton;
